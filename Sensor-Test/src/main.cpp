05df371ff9ed1796a8e5df7d59fddbddd879f641
Incapamentum <diazg.gustavo@knights.ucf.edu>
2022-03-27 17:00:34 -0400

    Completed working sample of DHT11 sensor

--- Sensor-Test/src/main.cpp
+++ Sensor-Test/src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
+#include <SimpleDHT.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define DHT11_PIN 2
+#define SECONDS 1.5
+#define T_DELAY SECONDS * 1000
+
+// Globals
+SimpleDHT11 dht11(DHT11_PIN);
+
+byte temperature;
+byte humidity;
+int err;
+
+void setup() 
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    Serial.println("=================================");
+    Serial.println("Sample DHT11...");
+
+    err = SimpleDHTErrSuccess;
+
+    if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+    {
+        Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+        Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(T_DELAY);
+        return;
+    }
+
+    Serial.print("Sample OK: ");
+    Serial.print((int)temperature); Serial.print(" *C, ");
+    Serial.print((int)humidity); Serial.println(" H");
+
+    // Sampling rate
+    delay(T_DELAY);
 }
