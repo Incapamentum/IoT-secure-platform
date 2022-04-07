d3c2cb458ebfc41cce6ff49cf071bde9684ae69c
Incapamentum <diazg.gustavo@knights.ucf.edu>
2022-04-07 17:55:22 -0400

    Started work towards testing out Transaction code

--- projects/Blockchain-Test/src/definitions.h
+++ projects/Blockchain-Test/src/definitions.h
@@ -1,10 +1,42 @@
 // Author: Gustavo Diaz Galeas (Incapamentum)
 //
-// Last revision: March 29th, 2022
+// Last revision: April 7th, 2022
 
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+#define MICROSEC 1000
+
 #define BAUD_RATE 9600
+#define POLL_RATE 10 * MICROSEC
+
+// NOTE: this may not be needed in the end
+// Week Days
+const String weekDays[7] = {
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+};
+
+// NOTE: this may not be needed in the end
+// Months
+const String months[12] = {
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+};
 
 #endif
--- projects/Blockchain-Test/src/main.cpp
+++ projects/Blockchain-Test/src/main.cpp
@@ -13,14 +13,26 @@
 #include <Arduino.h>
 #include <Crypto.h>
 #include <ESP8266WiFi.h>
-#include <SHA256.h>
+#include <NTPClient.h>
+#include <SimpleDHT.h>
 #include <Transactions.h>
+#include <WiFiUdp.h>
 
 #include "config.h"
 #include "definitions.h"
 
+#define DHT11_PIN 2
+
+//  ==============================
+//              Sensor
+// ===============================
+SimpleDHT11 dht11(DHT11_PIN);
+int err;
+// ===============================
+
 void setup() 
 {
+    
     Serial.begin(BAUD_RATE);
 
     Serial.println("\tSizes of specific structs/objects");
