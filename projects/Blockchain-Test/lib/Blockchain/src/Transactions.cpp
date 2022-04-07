6f10f422af9683743486494af567b0e0ed3fa606
Incapamentum <diazg.gustavo@knights.ucf.edu>
2022-04-07 17:26:00 -0400

    Completed(?) functionality of Transaction blocks

--- projects/Blockchain-Test/lib/Blockchain/src/Transactions.cpp
+++ projects/Blockchain-Test/lib/Blockchain/src/Transactions.cpp
@@ -4,6 +4,9 @@
 
 #include <algorithm>
 #include <Ed25519.h>
+#include <SHA256.h>
+#include <SHA512.h>
+#include <string.h>
 
 #include "Transactions.h"
 
@@ -24,31 +27,39 @@ void Transaction::setData(uint8_t temp, uint8_t hum)
 // Sets the timestamp_ variable to the value of ts, which is the result
 // of hashing the timestamp at the time of generation with the ID of
 // the chip
-void Transaction::setTimestamp(uint8_t ts[HASH_SIZE])
+void Transaction::setTimestamp(uint8_t ts[SHA1_SIZE])
 {
-    std::copy(ts, ts + HASH_SIZE, timestamp_);
+    std::copy(ts, ts + SHA1_SIZE, timestamp_);
+}
+
+// How to go on about hashing this?
+// More importantly, what goes in here for it to be hashed?
+// Timestamp and chip ID
+//  chipID is a 32-bit int (i.e. 4 bytes)
+// Hash size is 32 bytes (256-bits)
+void Transaction::hashTransaction(unsigned int chipId)
+{
+    SHA512 h;
+    unsigned int ptr = 0;
+    unsigned char d_buffer[transaction_size];
+
+    // Packing data to the buffer
+    memcpy(d_buffer + ptr, timestamp_, SHA1_SIZE);
+    ptr += SHA1_SIZE;
+    memcpy(d_buffer + ptr, serverKey_, KEY_LENGTH);
+    ptr += KEY_LENGTH;
+    memcpy(d_buffer + ptr, &data_, DATA_LENGTH);
+
+    // Setting data to be hashed
+    h.update(d_buffer, transaction_size);
+
+    // Hashing
+    h.finalize(transactionHash_, SHA512_SIZE);
 }
 
 // Signs the hash of the Transaction with the device private key
 int Transaction::sign(uint8_t deviceKey[KEY_LENGTH])
 {
-    // =======================
-    //          NOTES
-    // =======================
-    //
-    //      Ed25519::sign() takes in five params:
-    //          uint8_t signture[64] - signature value
-    //          uint8_t privateKey[32] - private key used to sign message
-    //          uint8_t publicKey[32] - public key to the corresponding private key
-    //          void *message - message to be signed
-    //          size_t len - length of the message to be signed
-
-    // Ed25519::sign(signature_, )
-    return 0;
-}
-
-// WiP
-int Transaction::verify(void)
-{
+    Ed25519::sign(signature_, deviceKey, serverKey_, transactionHash_, SHA512_SIZE);
     return 0;
 }
