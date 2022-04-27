1b1405201c11093ecad5ab57adfaeb7982c9f1ce
Incapamentum <diazg.gustavo@knights.ucf.edu>
2022-04-27 13:21:51 -0400

    Set up for baseline testing

---
+++ projects/AWS-Baseline/include/definitions.h
@@ -0,0 +1,18 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define EST -5
+
+#define BUDGET 128
+#define DEFAULT_SIZE 256
+
+#define DHT11_PIN 2
+#define DHT_FAILURE 0
+#define DHT_SUCCESS 1
+
+#define HALF_SEC 500
+#define SEC_IN_MILLIS 1000
+#define SEC_IN_MICROS 1000000
+#define POLL_RATE 10 * SEC_IN_MILLIS
+
+#endif
--- projects/AWS-Baseline/src/main.cpp
+++ projects/AWS-Baseline/src/main.cpp
@@ -8,19 +8,96 @@
  * Author: Anthony Elder 
  * License: Apache License v2
  */
+
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <NTPClient.h>
+#include <SimpleDHT.h>
+#include <WiFiUdp.h>
 
 #include "aws.h"
 #include "config.h"
+#include "definitions.h"
 
 BearSSL::X509List client_crt(certificatePemCrt);
 BearSSL::PrivateKey client_key(privatePemKey);
 BearSSL::X509List rootCert(caPemCrt);
 
+//  ==============================
+//          PubSub Client
+// ===============================
+char payload[DEFAULT_SIZE];
 WiFiClientSecure wiFiClient;
 void msgReceived(char* topic, byte* payload, unsigned int len);
-PubSubClient pubSubClient(awsEndpoint, 8883, msgReceived, wiFiClient); 
+PubSubClient pubSubClient(awsEndpoint, 8883, msgReceived, wiFiClient);
+unsigned long lastPublish;
+unsigned long l;
+// ===============================
+
+// ===============================
+//              Sensor
+// ===============================
+SimpleDHT11 dht11(DHT11_PIN);
+byte temperature, humidity;
+int err, sample;
+// ===============================
+
+// ===============================
+//           Timestamps
+// ===============================
+char ts[23];
+char *ts_buffer;
+
+struct tm *ptm;
+// ===============================
+
+//  ==============================
+//            Wifi/NTP
+// ===============================
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+// ===============================
+
+long unsigned int timeStart;
+long unsigned int timeEnd;
+double timeElapsed;
+
+void getTime(void)
+{
+    time_t epochTime;
+    int currentYear;
+    int currentMonth;
+    int currentDay;
+    int currentHour;
+    int currentMinute;
+    int currentSecond;
+
+    // Clearing string of any data
+    memset(ts, '\0', sizeof(ts));
+
+    timeClient.update();
+    epochTime = timeClient.getEpochTime();
+    ptm = gmtime ((time_t *)&epochTime);
+
+    currentYear = ptm->tm_year + 1900;
+    currentMonth = ptm->tm_mon + 1;
+    currentDay = ptm->tm_mday;
+    currentHour = timeClient.getHours();
+    currentMinute = timeClient.getMinutes();
+    currentSecond = timeClient.getSeconds();
+
+    // Writing timestamp
+    snprintf(ts, sizeof(ts), "%d-%02d-%02dT%02d:%02d:%02d+%d",
+        currentYear, currentMonth, currentDay, currentHour, currentMinute, currentSecond, EST);
+}
+
+void ntpInit(void)
+{
+    // Initializing NTP client
+    timeClient.begin();
+    timeClient.setTimeOffset(EST);
+}
 
 // Configures the ESP8266 time with the current time via
 // NTP access
@@ -76,15 +153,50 @@ void msgReceived(char* topic, byte* payload, unsigned int length)
     Serial.println();
 }
 
+int sampleSensor(void)
+{
+    Serial.println("=================================");
+    Serial.println("Sample DHT11...");
+
+    err = SimpleDHTErrSuccess;
+
+    if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+    {
+        Serial.println("DHT11 read failure");
+        return DHT_FAILURE;
+    }
+
+    Serial.println("Sample OK");
+    Serial.printf("Sample Num: %d\n", sample++);
+    return DHT_SUCCESS;
+}
+
+void wifiInit(void)
+{
+    // Connecting to Wifi
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(HALF_SEC);
+        Serial.print(".");
+    }
+
+    Serial.printf("\nWiFi connected, IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup()
 {
+    sample = 1;
     Serial.begin(115200); Serial.println();
-    Serial.println("ESP8266 AWS IoT Example");
+    Serial.println("ESP8266 AWS Baseline Experiment");
 
-    Serial.print("Connecting to "); Serial.print(ssid);
-    WiFi.begin(ssid, password);
-    WiFi.waitForConnectResult();
-    Serial.print(", WiFi connected, IP address: "); Serial.println(WiFi.localIP());
+    // Initializations
+    wifiInit();
+    ntpInit();
 
     // get current time, otherwise certificates are flagged as expired
     setCurrentTime();
@@ -93,18 +205,50 @@ void setup()
     wiFiClient.setTrustAnchors(&rootCert);
 }
 
-unsigned long lastPublish;
-int msgCount;
-
 void loop() 
 {
+    StaticJsonDocument<BUDGET> doc;
+    size_t size;
+
+    // Resetting
+    timeStart = 0;
+    timeEnd = 0;
+
     pubSubCheckConnect();
 
-    if (millis() - lastPublish > 10000)
+    if (sampleSensor() == DHT_SUCCESS)
     {
-        String msg = String("Hello from ESP8266: ") + ++msgCount;
-        pubSubClient.publish("outTopic", msg.c_str());
-        Serial.print("Published: "); Serial.println(msg);
-        lastPublish = millis();
+        // Start timing
+        timeStart = micros();
+
+        getTime();
+
+        // Clearing payload contents
+        memset(payload, '\0', DEFAULT_SIZE);
+
+        doc["temperature"] = temperature;
+        doc["humidity"] = humidity;
+        doc["timestamp"] = ts;
+
+        size = serializeJson(doc, Serial);
+
+        // Publishing
+        if (millis() - lastPublish > 10000)
+        {
+            pubSubClient.publish("outTopic", payload, size);
+            Serial.println("\nData was published");
+            Serial.printf("Size of doc: %d\n", size);
+            lastPublish = millis();
+        }
+
+        timeEnd = micros();
     }
+
+    if ((timeStart != 0) && (timeEnd != 0))
+    {
+        timeElapsed = (double) timeEnd - timeStart;
+        Serial.printf("Elapsed time: %.04f seconds\n", (double) timeElapsed / SEC_IN_MICROS);
+    }
+
+    delay(POLL_RATE);
 }
