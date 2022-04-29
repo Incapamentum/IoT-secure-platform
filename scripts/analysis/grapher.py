5a8d1a76b982c5083f5c13691fb1e1f7670c28c0
Incapamentum <diazg.gustavo@knights.ucf.edu>
2022-04-28 21:51:20 -0400

    Updated graph images

--- scripts/analysis/grapher.py
+++ scripts/analysis/grapher.py
@@ -5,11 +5,11 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 
+OUTPUT_FILE = "..\\output\\Latencies.png"
+
 if (len(sys.argv) != 3):
     print("Insufficient args")
     sys.exit(-1)
-
-labels = ("Baseline", "Transactions")
 
 baseline_path = sys.argv[1]
 transaction_path = sys.argv[2]
@@ -20,31 +20,24 @@ pickled_transaction = open(transaction_path, "rb")
 baseline_data = pickle.load(pickled_baseline)
 transaction_data = pickle.load(pickled_transaction)
 
-# baseline_data = log(baseline_data)
-# transaction_data = log(transaction_data)
-
-data = [baseline_data, transaction_data]
-
 # Plotting figures
-fig = plt.figure()
+fig, axes = plt.subplots(nrows=1, ncols=2)
 fig.suptitle("Data Collection & Publishing Latencies (1000 Samples)")
 
-ax = fig.add_subplot(111)
+axes[0].set_yscale("log")
+axes[0].set_ylabel("Latency (seconds)")
+axes[0].boxplot(baseline_data)
+axes[0].title.set_text("Baseline")
+axes[0].xaxis.set_ticklabels([])
 
-ax.set_yscale("log")
-ax.set_ylabel("Latency (seconds)")
-ax.set_xticklabels(labels)
+axes[1].set_yscale("log")
+# axes[1].set_ylabel("Latency (seconds)")
+axes[1].boxplot(transaction_data)
+axes[1].title.set_text("Transactions")
+axes[1].xaxis.set_ticklabels([])
 
-ax.boxplot(data)
+fig.tight_layout()
+plt.savefig(OUTPUT_FILE)
 
-plt.show()
-
-# plt.yscale("log")
-# plt.figure(figsize=(5, 5))
-# plt.yscale("log")
-# plt.ylabel("Latency (seconds)")
-# plt.xticks(np.arange(len(labels)) + 1, labels)
-
-# plt.boxplot(data)
-# plt.title("Data Collection & Publishing Latencies")
-# plt.show()
+pickled_baseline.close()
+pickled_transaction.close()
